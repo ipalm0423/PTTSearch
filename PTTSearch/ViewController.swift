2629f0c99b60ac4365214b368fbd729179482e53
ipalm0423 <76423ok@gmail.com>
2015-07-12 21:25:52 +0800

    Initial Commit

---
+++ PTTSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PTTSearch
+//
+//  Created by 陳冠宇 on 2015/7/12.
+//  Copyright (c) 2015年 陳冠宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
